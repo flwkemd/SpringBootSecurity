8fc93186f37eecfb17203943c7e6bc4c93e3ad9c
flwkemd <flwkemd@naver.com>
2017-11-13 23:02:34 +0100

    implemented Security & database

---
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.service;
+
+import com.example.model.User;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public User findUserByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
